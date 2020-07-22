8b811913ded641416c045e76f69619e45d0e9128
wenpandong <wen18408253283@163.com>
2020-07-22 16:51:20 +0800

    jdsljfdsa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '文盘东的私人领域'
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
+  // 去掉地址栏中的'#'
+  mode: 'history',
   routes: [
     {
       path: '/',
